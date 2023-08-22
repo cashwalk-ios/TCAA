9999788d5e776785a0f951abef5fc68898d6c422
Martin <kim.suwan@cashwalk.io>
2023-08-22 11:50:26 +0900

    Update create project

---
+++ TCAA/TCAA/TCAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAAApp.swift
+//  TCAA
+//
+//  Created by 김수완 on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TCAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
